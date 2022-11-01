55ea6f08ee4e028efa10b407eb16cb6cbec2b75e
mariusfa <mariusfagerland@yahoo.no>
2022-11-01 21:49:59 +0100

    Add demo app

---
+++ src/main/kotlin/com/example/demo/HelloWorldController.kt
@@ -0,0 +1,15 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HelloWorldController {
+
+    @GetMapping
+    fun getHello(): String {
+        return "hello"
+    }
+}
